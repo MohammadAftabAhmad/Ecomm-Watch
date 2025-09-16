7c2fe0afa6ae5f252ca32b0859a7056f46ad60b6
MOhammad Aftab Ahmad <mohammadaftabahmad56@gmail.com>
2025-09-16 19:15:37 +0530

    adding trending

--- src/pages/Home/Mwcard.tsx
+++ src/pages/Home/Mwcard.tsx
@@ -1,29 +1,53 @@
 
 import img from "../../assets/images/herom.jpeg"
 import img2 from "../../assets/images/herow.jpeg"
+import { NewArrivals } from "../../data/watchData"
 
 function MwCard() {
+
+
+
   return (
     <>
     
-     <div className='flex  px-10 py-10 bg-slate-100 mx-14 justify-center gap-5  items-center'>
+     <div className='flex  px-10 py-10 bg-slate-100 mx-14 justify-center gap-5 rounded-md items-center'>
       <div className="relative">
        <img className="w-[800px] h-[650px] rounded-md " src={img} alt="" />
         <div className="absolute bottom-4 left-3 flex ">
-          <button className=" px-5 py-2 bg-slate-800 text-white rounded-md hover:bg-black ">Explore</button>
-        </div>
+          <button className=" px-5 py-2 bg-black text-white rounded-md hover:bg-gray-800 ">Explore</button>
+        </div> 
        </div>
        <div className="relative">
        <img className="w-[800px] h-[650px] rounded-md " src={img2} alt="" />
         <div className="absolute bottom-4 left-3 flex ">
-          <button className=" px-5 py-2 bg-slate-800 text-white rounded-md  hover:bg-black">Explore</button>
+          <button className=" px-5 py-2 bg-black text-white rounded-md  hover:bg-gray-800">Explore</button>
         </div>
        </div>
-
+</div>
        
+
+
+       <div className="">
+
+          <h1 className="text-2xl font-semibold px-20 py-5">TRENDING NOW</h1>
+
+         </div>
+         <div className="px-20  grid gap-6 grid-cols-4">
+          {NewArrivals.map((w,id)=>(
+            <div key={id}>
+              <div className="bg-slate-100 rounded-lg">
+              <img className=" rounded-md hover:scale-105 w-80 duration-300 h-96" src={w.img} alt="" /></div>
+              <div>
+                <h1 className="text-gray-400">{w.name}</h1>
+                <p>{w.price}</p>
+              </div>
+            </div>
+          
+          ))}
+         </div>
         
     
-</div>
+
     </>
   )
 }
