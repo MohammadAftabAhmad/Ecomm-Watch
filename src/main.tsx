7f15c48e6b5e81e2708819d260555758301e2d63
MOhammad Aftab Ahmad <mohammadaftabahmad56@gmail.com>
2025-09-24 17:14:36 +0530

    slice

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { store,  } from "../src/redux/store"
+import { Provider } from 'react-redux'
 
 import { RouterProvider } from 'react-router-dom'
 import Router from "./Routes/index";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider store={store}>
   <RouterProvider router={Router}/>
+  </Provider>
+
   </StrictMode>,
 )
