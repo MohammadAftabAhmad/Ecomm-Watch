b78f58a75ae8d257dca64f00289b5857bf2746fd
MOhammad Aftab Ahmad <mohammadaftabahmad56@gmail.com>
2025-10-01 22:57:06 +0530

    updating hero section

--- src/pages/Home/HeroPage.tsx
+++ src/pages/Home/HeroPage.tsx
@@ -1,13 +1,26 @@
-import video from "../../assets/videos/hero.mp4"
+import img from "../../assets/images/background.png";
+import watches from "../../assets/images/watches.png";
+
 function HeroPage() {
   return (
-   <>
-    <div className="mx-auto  w-full top-0 ">
-        <video className="h-screen w-full  object-cover" autoPlay muted loop src={video}></video>
-    </div>
-   
-   </>
-  )
+    <>
+      <div className="relative w-full h-screen top-0 overflow-hidden ">
+        {/* Background image */}
+        <img
+          className="w-full h-full object-cover"
+          src={img}
+          alt="Background"
+        />
+
+        {/* Watches image at top right */}
+        <img
+          className="absolute top-1/2 right-10 -translate-y-1/2 z-10 w-[70%] object-contain opacity-100 "
+          src={watches}
+          alt="Watches"
+        />
+      </div>
+    </>
+  );
 }
 
-export default HeroPage
+export default HeroPage;
